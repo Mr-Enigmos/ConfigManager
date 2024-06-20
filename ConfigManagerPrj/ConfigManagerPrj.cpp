f918b9b1280ff93cce78721e204eeb5bfdd97b11
vit <vit@ENIGMA>
2024-06-20 21:46:05 +0300

    Добавьте файлы проекта.

---
+++ ConfigManagerPrj/ConfigManagerPrj.cpp
@@ -0,0 +1,12 @@
+﻿// ConfigManagerPrj.cpp: определяет точку входа для приложения.
+//
+
+#include "ConfigManagerPrj.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
